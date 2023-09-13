9447e87ad6f1482ecc67635aedb02b00ab02b26b
Your Name <you@example.com>
2023-09-13 21:06:26 +0530

    Commit Devbranch.tf

---
+++ DevBranch.tf
@@ -0,0 +1,16 @@
+# //Gets data for CURRENT location
+
+# data "aws_ami" "my_ami" {
+#     most_recent = true
+#     owners = ["amazon"]
+
+#     filter {
+#         name = "name"
+#         values = ["al2023-ami-2023*"]
+#     }
+# }
+
+# resource aws_instance "myinst" {
+#     ami = data.aws_ami.my_ami.id
+#     instance_type = "t2.micro"
+# }
